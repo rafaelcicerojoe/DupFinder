5d1111c2b131e723c35aeffa4b488b6d9915e84b
Fernando <fernandohenrique@nca.ufma.br>
2021-02-28 11:27:54 -0300

    [CREATE] - Serviço windows através da herança da classe SMWinservice()
    Funcionalidade faz uso da biblioteca pywin32 com módulo win32serviceutil.ServiceFramework.

---
+++ DupFinder.py
@@ -0,0 +1,41 @@
+import time
+import json
+from controls.smwinservice import SMWinservice
+from controls.duplicate_finder import DuplicateFinder
+
+
+class DupFinder(SMWinservice):
+    _svc_name_ = "DupFinder"
+    _svc_display_name_ = "DupFinderPython Service Example"
+    _svc_description_ = "O serviço DupFinder realiza o escaneamento de diretórios e subdiretórios em busca de " \
+                        "arquivos duplicados por tamanho e hash, assim como pastas vazias afim de realizar a exclusão" \
+                        " segura. "
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.is_running = False
+
+    def start(self):
+        self.is_running = True
+
+    def stop(self):
+        self.is_running = False
+
+    def main(self):
+        with open('configs/configs.json') as i:
+            _config = json.load(i)
+
+        dup_finder = DuplicateFinder(_config['directory'], _config['log_directory'], _config['hash_algorithm'],
+                                     _config['to_trash'], _config['deletion_mode'])
+
+        while self.isrunning:
+            dict_by_size = dup_finder.find_duplicate_by_size()
+            dict_by_hash = dup_finder.find_duplicate_by_full_hash(dict_by_size)
+            dup_finder.send_duplicate_to_trash(dict_by_hash)
+            dup_finder.remove_empty_folder(_config['directory'])
+            dup_finder.export_log()
+            time.sleep(30)
+
+
+if __name__ == '__main__':
+    DupFinder.parse_command_line()
